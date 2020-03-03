fd07114dad767247519c4f503d821543e610bc5a
Nikita Sandhu <sandhunikita0096@gmail.com>
2020-03-03 18:46:06 -0500

    sending data from fvc to svc

---
+++ Day3iOSPassingDataToViewController/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day3iOSPassingDataToViewController
+//
+//  Created by MacStudent on 2020-03-03.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+    @IBOutlet weak var textName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnGoNext(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Day3iOSPassingDataToViewController/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  Day3iOSPassingDataToViewController
+//
+//  Created by MacStudent on 2020-03-03.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    var name : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let nm = name{
+            self.lblWelcome.text = "Welcome, \(nm)"
+        }else{
+            self.lblWelcome.text = "No Name set from First VC"
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
